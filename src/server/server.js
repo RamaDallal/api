ed4ba878e4e0f5c544439c981e5090fb1f87f836
noursammour <nour.sammour@hotmail.com>
2016-12-27 01:43:37 +0300

    fix corns issue

--- src/server/server.js
+++ src/server/server.js
@@ -8,12 +8,13 @@ import express from 'express';
 import Schema from './graphql/rootSchema';
 import setupDB from './database/setupDB';
 import config from './../../config';
+const cors = require('cors');
 
 const home = (req: Object, res: Object): Object => res.sendStatus(200);
 
 setupDB(config.db);
 
 express()
-  .use('/api/graphql', graphqlHTTP({ schema: Schema, graphiql: true, pretty: true, raw: true }))
+  .use('/api/graphql', cors(), graphqlHTTP({ schema: Schema, graphiql: true, pretty: true, raw: true }))
   .use('/*', home)
   .listen(3030);
