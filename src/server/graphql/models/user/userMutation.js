7132ec3682b77bc1acecbc1214393185fc03375e
noursammour <nour.sammour@hotmail.com>
2017-01-10 16:46:33 +0300

    init emails

--- src/server/graphql/models/user/userMutation.js
+++ src/server/graphql/models/user/userMutation.js
@@ -6,67 +6,50 @@ import {
   GraphQLObjectType as ObjectType,
   GraphQLString as StringType,
 } from 'graphql';
-import config from '../../../../../config.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import nodemailer from 'nodemailer';
-import sgTransport from 'nodemailer-sendgrid-transport';
-import hbs from 'nodemailer-express-handlebars';
+import {signUpConfirmEmail} from '../../../email-service/emailService';
 
 export default {
   signup: {
-    type: User,
+    type: new ObjectType({
+      name: 'CreateUserResult',
+      fields: {
+        errors: { type: new List(StringType) },
+        user: { type: User },
+        token: { type: GraphQLString }
+      }
+    }),
     args: {
       email: { type: GraphQLString },
       password: { type: GraphQLString },
       isAuthenticated: { type: GraphQLString }
     },
-    resolve: (_, args: Object): Object => {
+    resolve: (_, args: Object): Object => new Promise((resolve) => {
       const salt = bcrypt.genSaltSync(10);
       const hash = bcrypt.hashSync(args.password, salt);
       const user = new UserModel();
       user.email = args.email;
       user.password = hash;
       user.isAuthenticated = false;
-      const options = {
-        auth: {
-          api_key: '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'
-        }
-      };
-      const client = nodemailer.createTransport(sgTransport(config.options));
-      const nodeMailerOptions = {
-        options: {
-          auth: {
-            api_key: config.nodeMailer.auth.api_key
-          }
-        }
-      };
-      const client = nodemailer.createTransport(sgTransport(config.nodeMailer));
-      const link = 'http://' + config.apiHost + ':' + config.apiPort + '/api/graphql/confirm?id=' + user.id;
-      const email = {
-        from: 'awesome@bar.com',
-        to: [args.email, 'sammour.ma7moud@gmail.com'],
-        subject: 'Hello',
-        text: 'Hello {{username}}',
-        template: 'template',
-        context: {
-          variable1 : '<b><a href="' + link + '">Confirm Link</a> </b>',
-        }
-      };
-      client.sendMail(email, function (error) {
-        if (error) {
-          console.log(error);
-        }
-        else {
-          console.log('Every thing is Oky');
+      signUpConfirmEmail(user, (err) => {
+        if (!err) {
+          user.save((userErr, userInfo) => {
+            if (!userErr) {
+              resolve({ user: userInfo });
+            } else {
+              resolve({ errors: [userErr] });
+            }
+          });
+        } else {
+          resolve({ errors: [err] });
         }
       });
-      return user.save();
-    }
+    })
   },
   login: {
     type: new ObjectType({
-      name: 'CreateUserResult',
+      name: 'LoginUserResult',
       fields: {
         errors: { type: new List(StringType) },
         user: { type: User },
