76e503339cf6f87ff4e05e06fb1781a4ce07a60d
ma7moud <sammour.ma7moud@gmail.com>
2017-01-10 14:20:49 +0300

    change password

--- src/server/graphql/models/user/userMutation.js
+++ src/server/graphql/models/user/userMutation.js
@@ -52,10 +52,60 @@ export default {
             const token = jwt.sign({ id: user.id }, 'super_secret');
             res = {
               user,
-              token,
+              token
             };
           }
           return resolve(res);
+        });
+      });
+    })
+  },
+  change: {
+    type: new ObjectType({
+      name: 'ChangePasswordResult',
+      fields: {
+        errors: { type: new List(StringType) },
+        user: { type: User },
+        token: { type: GraphQLString }
+      }
+    }),
+    args: {
+      password: { type: GraphQLString },
+      newPassword: { type: GraphQLString },
+      token: { type: GraphQLString }
+    },
+    resolve: (_, args:Object):Object => new Promise((resolve) => {
+      console.log('1');
+      var decoded = jwt.decode(args.token, 'super_secret');
+      console.log('2');
+      console.log(decoded);
+      console.log('3');
+      const salt = bcrypt.genSaltSync(10);
+      console.log('4');
+      const hash = bcrypt.hashSync(args.newPassword, salt);
+      console.log('5');
+      UserModel.findOne({ _id: decoded.id }, function (err, user) {
+        console.log('8');
+        console.log('9');
+        console.log(user.password);
+        console.log(args.password);
+        return bcrypt.compare(args.password, user.password,(passwordError, result) => {
+          let res;
+          if (!result) {
+            res = { errors: ['Invalid password'] };
+          } else if (passwordError) {
+            res = { errors: [passwordError] };
+          } else  {
+            console.log('10');
+            UserModel.update({
+              _id: decoded.id
+            }, {password: hash}, function () {
+              res = {
+                user
+              };
+              return resolve(res);
+            });
+          }
         });
       });
     })
