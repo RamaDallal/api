e5515e47666dc5cf3442dc521eb583be197c8f5a
ma7moud <sammour.ma7moud@gmail.com>
2017-02-10 15:25:59 +0300

    add new app config in facebook developer for heroku production

--- src/server/server.js
+++ src/server/server.js
@@ -60,8 +60,6 @@ passport.use(new FacebookStrategy({
             throw err;
           return done(null, newUser);
         });
-        console.log('x');
-        console.log(profile);
       }
     });
   }
